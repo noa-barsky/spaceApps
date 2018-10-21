ef0fe2289e9c6a3b6625d6212b40c59de97dbcda
noa-barsky <barskyn@mcmaster.ca>
2018-10-21 16:14:39 -0400

    completed, small updates to come later

--- script.js
+++ script.js
@@ -1,46 +1,144 @@
-const theSun = document.querySelector(".sun");
-const mercury = document.querySelector(".mercury");
-const venus = document.querySelector(".venus");
-const earth = document.querySelector(".earth");
-const mars = document.querySelector(".mars");
-const jupiter = document.querySelector(".jupiter");
-const saturn = document.querySelector(".saturn");
-const uranus = document.querySelector(".uranus");
-const neptune = document.querySelector(".neptune");
+
+
+try {
+  var SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+  var recognition = new SpeechRecognition();
+}
+catch(e) {
+  console.error(e);
+
+}
+
+
+var noteTextarea = $('.inputField');
+
+
+var noteContent = '';
+
+
+recognition.continuous = true;
+
+
+recognition.onresult = function(event) {
+
+  var current = event.resultIndex;
+
+  var transcript = event.results[current][0].transcript;
+
+  var mobileRepeatBug = (current == 1 && transcript == event.results[0][0].transcript);
+
+  if(!mobileRepeatBug) {
+    noteContent += transcript;
+    noteTextarea.val(noteContent);
+  }
+  noteContent = noteContent.toLowerCase();
+  console.log(noteContent);
+ 
+  if (noteContent == "the sun" || noteContent == "sun") {
+    location.href = "sun.html";
+    noteTextarea.val("");
+  } else if (noteContent == "mercury" || noteContent == "planet mercury"){
+    location.href = "mercury.html";
+    noteTextarea.val("");
+  } else if (noteContent == "venus" || noteContent == "planet venus"){
+    location.href = "venus.html";
+    noteTextarea.val("");
+  } else if (noteContent == "earth" || noteContent == "planet earth"){
+    location.href = "earth.html";
+    noteTextarea.val("");
+  } else if (noteContent == "mars" || noteContent == "planet mars"){
+    location.href = "mars.html";
+    noteTextarea.val("");
+  } else if (noteContent == "jupiter" || noteContent == "planet jupiter"){
+    location.href = "jupiter.html";
+    noteTextarea.val("");
+  } else if (noteContent == "saturn" || noteContent == "planet saturn"){
+    location.href = "saturn.html";
+    noteTextarea.val("");
+  } else if (noteContent == "uranus" || noteContent == "planet uranus"){
+    location.href = "uranus.html";
+    noteTextarea.val("");
+  } else if (noteContent == "neptune" || noteContent == "planet neptune"){
+    location.href = "neptune.html";
+    noteTextarea.val("");
+  } else {
+    alert ("Sorry, that is not a planet");
+    noteTextarea.val("");
+  }
+  
+
+};
+
+
+recognition.onerror = function(event) {
+  if(event.error == 'no-speech') {
+    alert('No speech was detected. Try again.');  
+  };
+}
+
+
+
+
+$('#start-record-btn').on('click', function(e) {
+  if (noteContent.length) {
+    noteContent += ' ';
+  }
+  recognition.start();
+});
+
+
+$('#pause-record-btn').on('click', function(e) {
+  recognition.stop();
+
+});
+
+
+noteTextarea.on('input', function() {
+  noteContent = $(this).val();
+})
+console.log(noteContent);
+
+
+
+
 
 function redirection () {
-    theSun.addEventListener("click", function(){
-        location.href = "sun.html";
-    });
-    mercury.addEventListener("click", function(){
-        location.href = "mercury.html";
-    });
-    venus.addEventListener("click", function(){
-        location.href = "venus.html";
-    });
-    earth.addEventListener("click", function(){
-        location.href = "earth.html";
-    });
-    mars.addEventListener("click", function(){
-        location.href = "mars.html";
-    });
-    jupiter.addEventListener("click", function(){
-        location.href = "jupiter.html";
-    });
-    saturn.addEventListener("click", function(){
-        location.href = "saturn.html";
-    });
-    uranus.addEventListener("click", function(){
-        location.href = "uranus.html";
-    });
-    neptune.addEventListener("click", function(){
-        location.href = "neptune.html";
-    });
+  $('.sun').on('click', function() {
+    location.href = "sun.html";
+  });
+  $('.mercury').on('click', function() {
+    location.href = "mercury.html";
+  });
+  $('.venus').on('click', function() {
+    location.href = "venus.html";
+  });
+  $('.earth').on('click', function() {
+    location.href = "earth.html";
+  });
+  $('.mars').on('click', function() {
+    location.href = "mars.html";
+  });
+  $('.jupiter').on('click', function() {
+    location.href = "jupiter.html";
+  });
+  $('.saturn').on('click', function() {
+    location.href = "saturn.html";
+  });
+  $('.uranus').on('click', function() {
+    location.href = "uranus.html";
+  });
+  $('.neptune').on('click', function() {
+    location.href = "neptune.html";
+  });
 
 }
 
 function init () {
-    redirection();
+  redirection();
+
 }
 
 init();
+
+
+
